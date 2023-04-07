636c62f94f8a6842e62a03ac15a114e65a492d9d
Gopal.Nyaupane <gopal.nyaupane100@gmail.com>
2023-04-07 09:03:35 +0545

    update

---
+++ CollectionExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1FirstApp
+{
+    internal class CollectionExample
+    {
+        public static void Main()
+        {
+            //insert data in array
+            int[] arr = new int[5];
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[2] = 3;
+            arr[3] = 4;
+            arr[4] = 5;
+
+            Console.WriteLine(arr[2]);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            //List insert process
+            List<int> list = new List<int>();
+            list.Add(10);
+            list.Add(20);
+            list.Insert(0, 30);
+
+            List<int> list1 = new List<int>();
+            list1.Add(100);
+            list1.Add(200);
+            list1.Insert(0, 300);
+            list.AddRange(list1);
+
+        }
+    }
+
+}
---
+++ DogNew.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1FirstApp
+{
+    internal class DogNew
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Color { get; set; }
+    }
+    public class DogCollection
+    {
+        public static void Main()
+        {
+            DogNew lucie=new DogNew();
+            lucie.Id = 1;
+            lucie.Name = "lucie";
+            lucie.Age = 3;
+            lucie.Breed = "Husky";
+            lucie.Color = "White";
+            lucie.Gender = "Male";
+
+            DogNew kale = new DogNew();
+            kale.Id = 2;
+            kale.Name = "kale";
+            kale.Age = 1;
+            kale.Breed = "Husky";
+            kale.Color = "White";
+            kale.Gender = "Male";
+
+            List<DogNew> list = new List<DogNew>();
+            list.Add(lucie);
+            list.Add(kale);
+
+            DogNew result = list.Where(y => y.Age > 2).First();
+
+            int[] marks = new int[5] { 60, 70, 32, 54, 70 };
+            var StudentList=marks.OrderByDescending(x => x).ToArray();
+
+        }
+    }
+}
